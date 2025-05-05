337bcc4e1b1c17244bbc0a9eaf55d65088295941
Luca Antonelli <lucaantonelli1999@gmail.com>
2025-05-05 22:06:02 +0200

    fix(build): release config + version update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,17 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 android {
@@ -22,7 +31,6 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-
         // Flag to enable support for the new language APIs
         isCoreLibraryDesugaringEnabled = true
 
@@ -42,9 +50,19 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        //create("release") {
+            //keyAlias = keystoreProperties["keyAlias"] as String
+            //keyPassword = keystoreProperties["keyPassword"] as String
+            //storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            //storePassword = keystoreProperties["storePassword"] as String
+        //}
+    }
+
     buildTypes {
 
         release {
+            //signingConfig = signingConfigs.getByName("release")
             signingConfig = signingConfigs.getByName("debug")
             isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
